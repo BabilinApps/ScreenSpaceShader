e03fd6b87925b37fc241ae8abd64a0c8ede37000
Adrian Babilinski <ababilinski@gmail.com>
2016-11-22 09:54:37 -0500

    added project

---
+++ Assets/ScreenSpaceTexture/DemoCode/BackAndForth.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackAndForth : MonoBehaviour {
+
+    public float delta = 1.5f;  // Amount to move left and right from the start point
+    public float speed = 2.0f;
+    private Vector3 startPos;
+
+    // Use this for initialization
+    void Start () {
+        startPos = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 v = startPos;
+        v.x += delta * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+}
